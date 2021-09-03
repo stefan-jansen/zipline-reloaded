dfe3e27b1780407dd4fb840014b5a247bf4c8379
MBounouar <MBounouar@users.noreply.github.com>
2021-09-03 22:26:21 +0200

    remove unused options

--- setup.py
+++ setup.py
@@ -103,8 +103,6 @@ ext_modules = [
         name="zipline.lib.adjustment",
         sources=["src/zipline/lib/adjustment.pyx"],
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-        extra_compile_args=["-fopenmp"],
-        extra_link_args=["-fopenmp"],
     ),
     Extension(
         name="zipline.lib._factorize",
