b8aace02dc6183d873353d91ec8500f5ede750da
Stefan Jansen <stefan@applied-ai.com>
2025-06-18 09:41:33 -0400

    fixed formatting

--- src/zipline/pipeline/domain.py
+++ src/zipline/pipeline/domain.py
@@ -167,7 +167,8 @@ class EquityCalendarDomain(Domain):
         self._data_query_offset = (
             # add one minute because `open_time` is actually the open minute
             # label which is one minute _after_ market open...
-            data_query_offset - np.timedelta64(1, "m")
+            data_query_offset
+            - np.timedelta64(1, "m")
         )
         if data_query_offset >= datetime.timedelta(0):
             raise ValueError(
